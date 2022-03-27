fc7c9ad32aa06e8666bfa9aaf3b9c7f98908b0de
Acer <toxtayev.elmurod93@mail.ru>
2022-03-27 16:29:31 +0500

    salo

---
+++ resources/views/product.blade.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <a href="{{route('create.product')}}" class="btn btn-primary m-5">add product</a>
+    </div>
+    <div class="row">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>name</th>
+                    <th>price</th>
+                    <th>country</th>
+                    <th>image</th>
+                </tr>
+
+            </thead>
+            <tbody>
+                @foreach(\App\Models\Product::all() as $item)
+                    <tr>
+                        <td>{{$item->id}}</td>
+                        <td>{{$item->name}}</td>
+                        <td>{{$item->price}}</td>
+                        <td>{{$item->country}}</td>
+                        <td><img src="{{asset('storage/'.$item->image)}}" width="100" alt=""></td>
+                        <td>
+                            <form style="display: inline-block" action="{{route('delete.product')}}" method="post">
+                                @csrf
+                                @method('delete')
+                                <input type="hidden" name="id"  value="{{$item->id}}">
+                                <input type="submit" class="btn btn-info" value="delete">
+                            </form>
+
+                            <form style="display: inline-block" action="{{route('edit.product')}}" method="post">
+                                @csrf
+                                @method('put')
+                                <input type="hidden" name="id"  value="{{$item->id}}" method="post">
+                                <input type="submit" class="btn btn-info" value="edit">
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+
+</body>
+</html>
